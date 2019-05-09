e7c14b0ccc57bf670fa26d791f2d33d6dd99da88
Alyssa Lundgren <lundgrea@gmail.com>
2019-05-08 20:46:34 -0600

    Add min and max functionality

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 var userMin = document.getElementbyId("lower-range").value;
 var userMax = document.getElementbyId("upper-range").value;
-var p1name = document.getElementbyId("name1").value;
-var p1guess = document.getElementbyId("guess1").value;
-var p2name = document.getElementbyId("name2").value;
-var p2guess = document.getElementbyId("guess2").value;
+var setRange = document.getElementbyId("range-button");
+var p1Name = document.getElementbyId("name1").value;
+var p1Guess = document.getElementbyId("guess1").value;
+var p2Name = document.getElementbyId("name2").value;
+var p2Guess = document.getElementbyId("guess2").value;
+var submitGuesses = document.getElementbyId("button-submit");
+var resetAll = document.getElementybyId("button-reset");
+var clearGame = document.getElementbyId("button-clear");
 var randomNumber;
 
 
-function generateNumber (){
-  return Math.floor(Math.random() * 100 + 1) 
+function generateNumber (max, min){
+  return Math.floor(Math.random() * (+max - +min) + +min); 
 }
 
-randomNumber = generateNumber();
+randomNumber = generateNumber(userMax, userMin);
 
+
+
+setRange.addEventListener('click', function(){
+  if (userMin == "") {
+    userMin = 1;
+  }
+  if (userMax == "") {
+    userMax = 100;
+  }
+});
 
 
 
