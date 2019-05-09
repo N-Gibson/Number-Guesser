04a81849df6a2a662a7b141676e5dc9536866abf
Alyssa Lundgren <lundgrea@gmail.com>
2019-05-09 12:03:48 -0600

    Begin refactor css and html

--- main.js
+++ main.js
@@ -1,37 +1,44 @@
-var userMin = document.getElementbyId("lower-range").value;
-var userMax = document.getElementbyId("upper-range").value;
-var setRange = document.getElementbyId("range-button");
-var p1Name = document.getElementbyId("name1").value;
-var p1Guess = document.getElementbyId("guess1").value;
-var p2Name = document.getElementbyId("name2").value;
-var p2Guess = document.getElementbyId("guess2").value;
-var submitGuesses = document.getElementbyId("button-submit");
-var resetAll = document.getElementybyId("button-reset");
-var clearGame = document.getElementbyId("button-clear");
-var randomNumber;
+// var userMin = document.getElementbyId('lower-range').value;
+// var userMax = document.getElementbyId('upper-range').value;
+// var setRange = document.getElementbyId('range-button');
+var p1NameInput = document.getElementbyId('name-1');
+var p1Name = document.querySelector('.p1-rename');
+var submitGuesses = document.getElementbyId('button-submit');
+// var p1Guess = document.getElementbyId('guess1').value;
+// var p2Name = document.getElementbyId('name2').value;
+// var p2Guess = document.getElementbyId('guess2').value;
+// var resetAll = document.getElementybyId('button-reset');
+// var clearGame = document.getElementbyId('button-clear');
+// var randomNumber;
 
 
-function generateNumber (max, min){
-  return Math.floor(Math.random() * (+max - +min) + +min); 
-}
+// function generateNumber (max, min){
+//   return Math.floor(Math.random() * (+max - +min) + +min); 
+// }
 
-randomNumber = generateNumber(userMax, userMin);
+// randomNumber = generateNumber(userMax, userMin);
 
-
-
-setRange.addEventListener('click', function(){
-  if (userMin == "") {
-    userMin = 1;
-  }
-  if (userMax == "") {
-    userMax = 100;
-  }
+changeP1Name.addEventListener('click' function(){
+  var p1 = p1NameInput.value;
+  p1-rename.innerText = p1NameInput;
 });
 
+// setRange.addEventListener('click', function(){
+//   if (userMin == "") {
+//     userMin = 1;
+//   }
+//   if (userMax == "") {
+//     userMax = 100;
+//   }
+// });
 
 
 
 
 
-var challengerOneName = document.querySelector(#)
-button.addEventListener('click', fun)
+
+
+
+
+// var challengerOneName = document.querySelector(#)
+// button.addEventListener('click', fun)
