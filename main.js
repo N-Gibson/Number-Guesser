6418bd8dc82be2ee136bd4afe70f53b8dbba099c
Alyssa Lundgren <lundgrea@gmail.com>
2019-05-08 20:21:33 -0600

    Add all variables

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-var challengerOneName = document.querySelector(#)
-
-
+var userMin = document.getElementbyId("lower-range").value;
+var userMax = document.getElementbyId("upper-range").value;
+var p1name = document.getElementbyId("name1").value;
+var p1guess = document.getElementbyId("guess1").value;
+var p2name = document.getElementbyId("name2").value;
+var p2guess = document.getElementbyId("guess2").value;
 var randomNumber;
+
 
 function generateNumber (){
   return Math.floor(Math.random() * 100 + 1) 
 }
 
 randomNumber = generateNumber();
+
+
+
+
+
+
+
+var challengerOneName = document.querySelector(#)
+button.addEventListener('click', fun)
