f17ec96df6064c563e90ac389474a2e0296552e7
Noah Gibson <noah.gibson99@gmail.com>
2019-05-10 18:37:29 -0600

    Modify JS, working on random number generator

--- main.js
+++ main.js
@@ -1,30 +1,96 @@
-var userMin = document.getElementbyId('lower-range').value;
-var userMax = document.getElementbyId('upper-range').value;
+// var userMin = document.getElementyById('#lower-range').value;
+// var userMax = document.getElementbyId('upper-range').value;
 var setRange = document.getElementbyId('range-button');
-var p1NameInput = document.getElementbyId('name-1').value;
-// var p1Name = document.querySelector('.p1-rename');
-var submitGuesses = document.getElementbyId('button-submit');
-var p1Guess = document.getElementbyId('guess-1').value;
-var p2Name = document.getElementbyId('name-2').value;
-var p2Guess = document.getElementbyId('guess-2').value;
-var resetAll = document.getElementybyId('button-reset');
-var clearGame = document.getElementbyId('button-clear');
-var randomNumber;
+// var p1NameInput = document.getElementbyId('name-1').value;
+// // var p1Name = document.querySelector('.p1-rename');
+// var submitGuesses = document.getElementbyId('button-submit');
+// var p1Guess = document.getElementbyId('guess-1').value;
+// var p2Name = document.getElementbyId('name-2').value;
+// var p2Guess = document.getElementbyId('guess-2').value;
+// var resetAll = document.getElementybyId('button-reset');
+// var clearGame = document.getElementbyId('button-clear');
+// var randomNumber;
 
 
-function generateNumber (max, min){
-  return Math.floor(Math.random() * (+max - +min) + +min); 
-}
-
-randomNumber = generateNumber(userMax, userMin);
+// function generateNumber (max, min){ 
+//   return Math.floor(Math.random() * (+max - +min) + +min); 
+// }
 
 
+setRange.addEventListener('click', function(){
+  if userMin 
 
 
-changeP1Name.addEventListener('click' function(){
-  var p1 = p1NameInput.value;
-  p1-rename.innerText = p1NameInput;
-});
+
+    function generateRandomNumber (0, 100) {
+    randomNumber = Math.floor(Math.random() * (max - min) + 1)
+    console.log(randomNumber)
+  else {
+    function generateRandomNumber (min, max){
+    randomNumber = Math.floor(Math.random() * (max - min) + min + 1)
+    console.log(randomNumber)
+  }
+})
+
+
+
+
+
+    function generateRandomNumber(min, max){
+    randomNumber = Math.floor(Math.random() * (max - min) + min + 1)
+    console.log(randomNumber)
+  }
+})
+
+
+
+function generateRandomNumber(min, max) {
+  randomNumber = Math.floor(Math.random() * (max - min) + min + 1 )
+  console.log(randomNumber)
+  }
+
+generateRandomNumber(userMin, userMax)
+
+if userMin = null
+
+
+
+function generateNumber ()[
+]
+// randomNumber = generateNumber(userMax, userMin);
+
+// function generateNumber (){ 
+//   randomNumber = Math.floor(Math.random() * 101);
+//   console.log randomNumber
+// } 
+// generateNumber ()
+
+
+
+// function generateNumber (min,max){ 
+//   randomNumber = Math.floor(Math.random() * - 1);
+//   console.log(randomNumber);
+// }; 
+
+
+// generateNumber(userMin, userMax);
+
+
+
+
+// changeP1Name.addEventListener('click' function(){
+//   var p1 = p1NameInput.value;
+//   p1-rename.innerText = p1NameInput;
+// });
+
+
+// var = generateNumber (max,min) {
+//   if (maxNum <1)
+
+// }
+
+
+
 
 // setRange.addEventListener('click', function(){
 //   if (userMin == "") {
@@ -45,3 +111,20 @@ changeP1Name.addEventListener('click' function(){
 
 // var challengerOneName = document.querySelector(#)
 // button.addEventListener('click', fun)
+
+
+
+
+
+
+var updateGame = document.querySelector('.min-max');
+
+updateGame.addEventListener('click', function (){
+
+})
+
+function diasbleButton() {
+  ;
+
+}
+
