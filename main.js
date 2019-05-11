833652476f20f7507b899847a670461fc7ac90b4
Noah Gibson <noah.gibson99@gmail.com>
2019-05-11 12:05:21 -0600

    Update button functional
    CO-authored-by: Alyssa Lundgren <lundgrea@gmail.com>

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-
-
 function generateRandomNumber(min,max) {
   var randomNumber = Math.floor(Math.random() * (max - min) + 1);
   console.log(randomNumber);
 };
 
 generateRandomNumber (1,100);
+
+
+
+
+var userMin = document.getElementById('lower-range');
+var userMax = document.getElementById('upper-range');
+var updateButton = document.getElementById('range-button');
+var displayMinNumber = document.getElementById('display-min');
+var displayMaxNumber = document.getElementById('display-max');
+
+updateButton.addEventListener('click', function (){
+  event.preventDefault();
+  displayMinNumber.innerText = userMin.value;
+  displayMaxNumber.innerText = userMax.value;
+  generateRandomNumber(userMin.value, userMax.value);
+});
