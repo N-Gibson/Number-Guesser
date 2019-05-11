edfb969243ff9a2abb3775c627337581065dd8ef
Alyssa Lundgren <lundgrea@gmail.com>
2019-05-11 14:14:26 -0600

    Integrate challenger names across page once input
    Co-authored-by: Noah Gibson <noah.gibson99@gmail.com>

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 function generateRandomNumber(min,max) {
-  var randomNumber = Math.floor(Math.random() * (max - min) + 1);
+  var randomNumber = Math.floor(Math.random() * (max - min) + min);
   console.log(randomNumber);
 };
 
-generateRandomNumber (1,100);
+var minAutomatic = 1;
+var maxAutomatic = 100;
 
+generateRandomNumber (minAutomatic, maxAutomatic);
 
+//end random number generator//
 
+//start set range//
 
 var userMin = document.getElementById('lower-range');
 var userMax = document.getElementById('upper-range');
@@ -14,9 +18,39 @@ var updateButton = document.getElementById('range-button');
 var displayMinNumber = document.getElementById('display-min');
 var displayMaxNumber = document.getElementById('display-max');
 
-updateButton.addEventListener('click', function (){
+updateButton.addEventListener('click', rangeGenerator);
+
+function rangeGenerator (){
   event.preventDefault();
   displayMinNumber.innerText = userMin.value;
   displayMaxNumber.innerText = userMax.value;
-  generateRandomNumber(userMin.value, userMax.value);
-});
+  generateRandomNumber (Number(userMin.value), Number(userMax.value));
+};
+
+//end set range//
+
+//start accept guesses//
+
+var p1name = document.getElementById('name-1');
+var p2name = document.getElementById('name-2');
+var p1guess = document.getElementById('guess-1');
+var p2guess = document.getElementById('guess-2');
+var submitButton = document.getElementById('button-submit');
+var p1Everywhere = document.querySelectorAll('.p1-rename');
+var p2Everywhere = document.querySelectorAll('.p2-rename');
+
+submitButton.addEventListener('click', compareGuesses);
+
+function compareGuesses (){
+  event.preventDefault();
+  // p1Everywhere = [];
+  for (var i = 0; i < p1Everywhere.length; i++){
+    p1Everywhere[i].innerText = p1name.value;
+  };
+  // p2Everywhere = [];
+  for (var i = 0; i <p2Everywhere.length; i++){
+    p2Everywhere[i].innerText = p2name.value;
+  };
+};
+
+
